d661e266d2d88e8eddab6a744c985323d406e3fc
omerglck <omergolcek10@gmail.com>
2024-01-19 13:28:17 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+##
+
+- react-router-dom
+- react-select
+- tailwind
+- framer-motion
+# 7-hi-car-rental
---
+++ src/utils/index.tsx
@@ -0,0 +1,43 @@
+import { colors } from "../constants";
+import { CarType, filterType } from "../types";
+
+const options = {
+  headers: {
+    "X-RapidAPI-Key": "c0f6459d13mshcb2e3fd751905cfp150deejsn90c1210a75e4",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+export async function fetchCars(filters: filterType) {
+  const {
+    make = "bmw",
+    model = "",
+    limit = "",
+    year = "",
+    fuel = "",
+  } = filters;
+
+  const res = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${make}&model=${model}&fuel_type=${fuel}&year=${year}&limit=${limit}`,
+    options
+  );
+  return await res.json();
+}
+
+// fotoğraf url'i oluşturan bir method
+export const genereteImage = (car: CarType, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", car.make);
+  url.searchParams.append("modelFamily", car.model);
+  url.searchParams.append("zoomType", "fullscreen");
+
+  if (angle) {
+    url.searchParams.append("angle", angle);
+  }
+  // rastgele renk kodu seç
+  const i = Math.round(Math.random() * colors.length);
+  url.searchParams.append("paintId", colors[i]);
+
+  return String(url);
+};
